84d106b3b6a6d2ce68f469d688b9fdb40f8075ef
jaaunt <jaaunt@taltech.ee>
2025-09-01 21:06:56 +0300

    test

---
+++ P1/src/ee/taltech/algoritmid/fibonacci/P1.java
@@ -0,0 +1,32 @@
+package ee.taltech.algoritmid.fibonacci;
+
+public class P1 {
+
+    /**
+     * Compute the Fibonacci sequence number.
+     * @param n The number of the sequence to compute.
+     * @return The n-th number in Fibonacci series.
+     */
+    public String iterativeF(int n) {
+        if (n == 0) {
+            return "0";
+        }
+        if (n == 1) {
+            return "1";
+        }
+
+        int a = 0, b = 1; // initialiseerib nr
+
+
+        return String.valueOf(iterativeF(n - 1) + iterativeF(n - 2));
+    }
+
+    public static void main(String[] args) throws Exception {
+        P1 p1 = new P1();
+
+        if (!p1.iterativeF(3).equals("2")) {
+            throw new Exception("There is a mistake in your solution.");
+        }
+    }
+
+}
