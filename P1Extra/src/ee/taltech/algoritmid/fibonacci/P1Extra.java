2bc21b04b69f1472f0239e2b826cbb25ed836a78
jaaunt <jaaunt@taltech.ee>
2025-09-02 13:31:53 +0300

    P1Extra

---
+++ P1Extra/src/ee/taltech/algoritmid/fibonacci/P1Extra.java
@@ -0,0 +1,29 @@
+package ee.taltech.algoritmid.fibonacci;
+
+import java.math.BigInteger;
+
+public class P1Extra {
+
+    /**
+     * Compute the Fibonacci sequence number recursively.
+     * (You need this in the timeToComputeRecursiveFibonacci(int n) function!)
+     * @param n The n-th number to compute.
+     * @return The n-th Fibonacci number as a string.
+     */
+    public static BigInteger recursiveF(int n) {
+        if (n <= 1)
+            return BigInteger.valueOf(n);
+        return recursiveF(n - 1).add(recursiveF(n - 2));
+    }
+
+    /**
+     * Estimate or find the exact time required to compute the n-th Fibonacci number.
+     * @param n The n-th number to compute.
+     * @return The time estimate or exact time in YEARS.
+     */
+    public static String timeToComputeRecursiveFibonacci(int n) {
+        // TODO
+        return "";
+    }
+
+}
