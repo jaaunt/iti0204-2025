eb50b6cf02f3d546539861954e556bbd5fcc48f8
jaaunt <jaaunt@taltech.ee>
2025-09-02 13:59:35 +0300

    P1Extra

--- P1Extra/src/ee/taltech/algoritmid/fibonacci/P1Extra.java
+++ P1Extra/src/ee/taltech/algoritmid/fibonacci/P1Extra.java
@@ -1,5 +1,6 @@
 package ee.taltech.algoritmid.fibonacci;
 
+import javax.sound.midi.SysexMessage;
 import java.math.BigInteger;
 
 public class P1Extra {
@@ -22,8 +23,21 @@ public class P1Extra {
      * @return The time estimate or exact time in YEARS.
      */
     public static String timeToComputeRecursiveFibonacci(int n) {
-        // TODO
-        return "";
+        int base = 10;
+        if (n <= 1) return "0";
+
+        long start = System.nanoTime();
+        recursiveF(base);
+        long end = System.nanoTime();
+
+        long diff_sec = (end - start)/1_000_000_000;
+
+        long diff_year = diff_sec/(60*60*24*365);
+        return String.format("%.15f", diff_year);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(timeToComputeRecursiveFibonacci(10));
     }
 
 }
