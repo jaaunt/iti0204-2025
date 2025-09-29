b76534648a3579120b81a94d6cf5754c77773c12
jaaunt <jaaunt@taltech.ee>
2025-09-29 19:56:50 +0300

    p5

--- P5/src/ee/taltech/algoritmid/socialmedia/SocialMedia.java
+++ P5/src/ee/taltech/algoritmid/socialmedia/SocialMedia.java
@@ -7,9 +7,6 @@ public class SocialMedia {
     private final Map<String, Integer> visits = new HashMap<>();
     private static final Set<String> ALLOWED_HTTP = Set.of("GET", "POST", "PUT", "DELETE");
 
-    private int top1 = 0;
-    private int top2 = 0;
-
     private String makeKey(String url, String http) {
         return url + "|" + http;
     }
@@ -21,49 +18,53 @@ public class SocialMedia {
     }
 
     /**
-     * @param url  - website address
-     * @param http - http request method
+     * Lisab külastuse antud URL+HTTP kombinatsioonile
      */
     public void addVisit(String url, String http) {
         validateHttp(http);
         String key = makeKey(url, http);
-        int newCount = visits.getOrDefault(key, 0) + 1;
-        visits.put(key, newCount);
-
-        if (newCount > top1) {
-            top1 = newCount;
-            top2 = visits.values().stream()
-                    .filter(v -> v < top1)
-                    .max(Integer::compareTo)
-                    .orElse(0);
-        } else if (newCount > top2 && newCount < top1) {
-            top2 = newCount;
-        }
+        visits.put(key, visits.getOrDefault(key, 0) + 1);
     }
 
-
     /**
-     * @param url  - website address
-     * @param http - http request method
-     * @return the number of visits of the given website
+     * Tagastab külastuste arvu antud URL+HTTP kombinatsiooni kohta
      */
-    public Integer getNumberOfVisits(String url, String http) throws IllegalArgumentException {
+    public int getNumberOfVisits(String url, String http) {
         validateHttp(http);
         String key = makeKey(url, http);
         return visits.getOrDefault(key, 0);
     }
 
     /**
-     * @return the number of visits of the most visited website
+     * Tagastab kõige rohkem külastatud lehe külastuste arvu
      */
-    public Integer getTop1() {
+    public int getTop1() {
+        int top1 = 0;
+        for (int v : visits.values()) {
+            if (v > top1) {
+                top1 = v;
+            }
+        }
         return top1;
     }
 
     /**
-     * @return the list of the number of visits of the top 2 most visited websites sorted in descending order
+     * Tagastab top 2 külastuste arvu suurimas järjekorras.
+     * Kui vähem kui 2 lehte, täidetakse 0-ga.
      */
     public List<Integer> getTop2() {
+        int top1 = 0;
+        int top2 = 0;
+
+        for (int v : visits.values()) {
+            if (v > top1) {
+                top2 = top1;
+                top1 = v;
+            } else if (v > top2) {
+                top2 = v;
+            }
+        }
+
         return List.of(top1, top2);
     }
 }
