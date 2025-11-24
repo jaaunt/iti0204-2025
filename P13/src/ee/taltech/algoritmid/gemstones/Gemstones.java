bc3730952a1942cb391edd727323696b0b41f5b8
jaaunt <jaaunt@taltech.ee>
2025-11-24 23:21:49 +0200

    test

---
+++ P13/src/ee/taltech/algoritmid/gemstones/Gemstones.java
@@ -0,0 +1,12 @@
+package ee.taltech.algoritmid.gemstones;
+
+import java.util.List;
+
+public class Gemstones {
+
+    public static List<Integer> findGemstones(int sum, List<Integer> gemstoneOptions) throws IllegalArgumentException {
+        // TODO
+        return null;
+    }
+
+}
