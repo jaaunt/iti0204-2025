82afbba2dc97705f7ffa0b8c4c1d48229614a18c
jaaunt <jaaunt@taltech.ee>
2025-12-08 20:33:18 +0200

    vb? initialzie prrit ja liis

--- P15/src/ee/taltech/algoritmid/hr/P15.java
+++ P15/src/ee/taltech/algoritmid/hr/P15.java
@@ -1,21 +1,17 @@
 package ee.taltech.algoritmid.hr;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class P15 {
     public enum Department {
         CUSTOMER_SUPPORT, RESEARCH_AND_DEVELOPMENT, UNKNOWN;
     }
 
     private final DisjointSubsets disjointSubsets = new DisjointSubsets();
-    // milliste gruppide juurte kohta me teame osakonda
-    private Map<String, Department> departmentByRoot = new HashMap<>();
 
     public P15() {
-        // don't remove
+        // priit ja Liis on osakondade juhid
+        disjointSubsets.addSubset("Priit");
+        disjointSubsets.addSubset("Liis");
     }
-    // mis see veel on ^ wtf
 
     public DisjointSubsets getDisjointSubsets() {
         return disjointSubsets;
@@ -39,29 +35,8 @@ public class P15 {
             addPerson(name2);
         }
 
-        // salvestame vanad juurte osakonna info ENNE union'it
-        String root1 = disjointSubsets.find(name1);
-        String root2 = disjointSubsets.find(name2);
-
-        Department dept1 = departmentByRoot.get(root1);
-        Department dept2 = departmentByRoot.get(root2);
-
         // uhendame grupid
         disjointSubsets.union(name1, name2);
-
-        // leiame uue juure PARAST union'it
-        String newRoot = disjointSubsets.find(name1);
-
-        // maarame uuele juurele oige osakonna
-        // kui molemal oli osakond maaratud, peaks need olema samad
-        if (dept1 != null && dept1 != Department.UNKNOWN) {
-            departmentByRoot.put(newRoot, dept1);
-        } else if (dept2 != null && dept2 != Department.UNKNOWN) {
-            departmentByRoot.put(newRoot, dept2);
-        } else {
-            // kui kumbki pole teada, jaab UNKNOWN
-            departmentByRoot.put(newRoot, Department.UNKNOWN);
-        }
     }
 
 
@@ -69,45 +44,48 @@ public class P15 {
     public void addPerson(String name) {
         try {
             disjointSubsets.addSubset(name);
-            // alguses ei tea me osakonda
-            departmentByRoot.put(name, Department.UNKNOWN);
         } catch (IllegalArgumentException e) {
             // inimene on juba susteemis, ei tee midagi
         }
     }
 
     // maarab, et see inimene on CS osakonnast
+    // uhendame ta Priiduga (CS marker)
     public void setCustomerSupport(String name) {
-        // lisame inimese kui pole juba susteemis
         try {
-            disjointSubsets.addSubset(name);
+            disjointSubsets.find(name);
         } catch (IllegalArgumentException e) {
-            // inimene on juba susteemis
+            addPerson(name);
         }
-        // leiame juure ja maarame osakonna
-        String root = disjointSubsets.find(name);
-        departmentByRoot.put(root, Department.CUSTOMER_SUPPORT);
+        disjointSubsets.union(name, "Priit");
     }
 
     // maarab, et see inimene on R&D osakonnast
+    // uhendame ta Liisiga (R&D marker)
     public void setResearchAndDevelopment(String name) {
-        // lisame inimese kui pole juba susteemis
         try {
-            disjointSubsets.addSubset(name);
+            disjointSubsets.find(name);
         } catch (IllegalArgumentException e) {
-            // inimene on juba susteemis
+            addPerson(name);
         }
-        // leiame juure ja maarame osakonna
-        String root = disjointSubsets.find(name);
-        departmentByRoot.put(root, Department.RESEARCH_AND_DEVELOPMENT);
+        disjointSubsets.union(name, "Liis");
     }
 
     // tagastab inimese osakonna
+    // kontrollib, kas tema juur on sama mis Priidi voi Liisi juur
     public Department getDepartment(String name) {
         try {
-            String root = disjointSubsets.find(name);
-            Department dept = departmentByRoot.get(root);
-            return dept != null ? dept : Department.UNKNOWN;
+            String personRoot = disjointSubsets.find(name);
+            String priitRoot = disjointSubsets.find("Priit");
+            String liisRoot = disjointSubsets.find("Liis");
+
+            if (personRoot.equals(priitRoot)) {
+                return Department.CUSTOMER_SUPPORT;
+            }
+            if (personRoot.equals(liisRoot)) {
+                return Department.RESEARCH_AND_DEVELOPMENT;
+            }
+            return Department.UNKNOWN;
         } catch (IllegalArgumentException e) {
             // inimene pole susteemis
             return Department.UNKNOWN;
